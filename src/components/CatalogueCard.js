04cb52fdc3490cb0603a317cd9ea011eff28d905
Lartum <lraksap11@gmail.com>
2020-10-04 02:12:30 +0530

    optmising for mobile display

--- src/components/CatalogueCard.js
+++ src/components/CatalogueCard.js
@@ -18,7 +18,7 @@ export const CatalogueCard = ({props}) =>{
     hoverable
     className='catalogueImage'
     cover={ <img alt='plant' src = { imageLink } onLoad={()=> setLoading(false)}/>}
-    style={{ width: 300}}
+    // style={{ width: 300}}
     ><Meta 
         title={ plantName } description={[
         <div> 
